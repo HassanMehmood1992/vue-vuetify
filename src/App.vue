ad21aae91168d03867d67d3d504b700dd18b9417
Hassan Mehmood <hassan.mehmood@click.aero>
2019-04-23 10:48:46 +0400

    view updated

--- src/App.vue
+++ src/App.vue
@@ -25,43 +25,13 @@
         </v-btn>
       </v-toolbar>
     <v-navigation-drawer app v-model="drawer" class="pb-0" floating hide-overlay stateless width="380">
-      <v-layout fill-height>
-        <v-navigation-drawer dark mini-variant stateless value="true">
-          <v-toolbar flat class="transparent">
-            <v-list class="pa-0">
-              <v-list-tile avatar>
-                <v-list-tile-avatar>
-                  <img src="https://randomuser.me/api/portraits/men/85.jpg">
-                </v-list-tile-avatar>
+      <v-toolbar dark color="primary" flat>
+       
 
-                <v-list-tile-content>
-                  <v-list-tile-title>John Leider</v-list-tile-title>
-                </v-list-tile-content>
-
-                <v-list-tile-action>
-                  <v-btn icon @click.native.stop="mini = !mini">
-                    <v-icon>chevron_left</v-icon>
-                  </v-btn>
-                </v-list-tile-action>
-              </v-list-tile>
-            </v-list>
-          </v-toolbar>
-
-          <v-list class="pt-0" dense>
-            <v-divider></v-divider>
-
-            <v-list-tile app v-for="item in items" :key="item.title" @click>
-              <v-list-tile-action>
-                <v-icon>{{ item.icon }}</v-icon>
-              </v-list-tile-action>
-
-              <v-list-tile-content>
-                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list>
-        </v-navigation-drawer>
-
+        <v-toolbar-title class="white--text">Logs</v-toolbar-title>
+      </v-toolbar>
+      <v-layout >
+               
         <v-list class="grow">
           <v-list-tile v-for="link in links" :key="link" @click>
             <v-list-tile-title v-text="link"></v-list-tile-title>
@@ -69,6 +39,7 @@
         </v-list>
       </v-layout>
     </v-navigation-drawer>
+
     </nav>
     <v-content>
       <router-view/>
