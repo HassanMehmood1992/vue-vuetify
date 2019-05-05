a06dd595e3d1f7daf0b882bb89fe82933edb5abc
Hassan Mehmood <hassan.mehmood@click.aero>
2019-05-05 13:07:42 +0400

    mini drawer added

--- src/App.vue
+++ src/App.vue
@@ -24,18 +24,55 @@
           <v-icon>more_vert</v-icon>
         </v-btn>
       </v-toolbar>
-      <v-navigation-drawer  app v-model="drawer" class="pb-0" floating  >
-        <v-toolbar dark color="primary" flat>
-          <v-toolbar-title class="white--text">Logs</v-toolbar-title>
-        </v-toolbar>
-        <v-layout>
-          <v-list class="grow">
-            <v-list-tile v-for="link in links" :key="link" @click>
-              <v-list-tile-title v-text="link"></v-list-tile-title>
-            </v-list-tile>
-          </v-list>
-        </v-layout>
-      </v-navigation-drawer>
+      <!-- <v-navigation-drawer   :mini-variant.sync="mini" app v-model="drawer" class="pb-0" floating  > -->
+         <v-navigation-drawer
+    v-model="drawer"
+    app
+    :mini-variant.sync="mini"
+    
+    
+  >
+    <v-toolbar flat class="transparent" color="primary">
+      <v-list class="pa-0">
+        <v-list-tile avatar>
+          <v-list-tile-avatar>
+            <img src="./assets/test.jpg">
+          </v-list-tile-avatar>
+
+          <v-list-tile-content>
+            <v-list-tile-title class="white--text">Hassan Mehmood</v-list-tile-title>
+          </v-list-tile-content>
+
+          <v-list-tile-action>
+            <v-btn
+              icon
+              @click.stop="mini = !mini"
+            >
+              <v-icon class="white--text">chevron_left</v-icon>
+            </v-btn>
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
+    </v-toolbar>
+
+    <v-list class="pt-0" dense>
+      <v-divider></v-divider>
+
+      <v-list-tile
+        v-for="item in items"
+        :key="item.title"
+        @click=""
+      >
+        <v-list-tile-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-tile-action>
+
+        <v-list-tile-content>
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
     </nav>
     <v-content>
       <router-view/>
@@ -59,7 +96,7 @@ export default {
         { title: "About", icon: "question_answer" }
       ],
       links: ["Home", "Contacts", "Settings"],
-      mini: true,
+      mini: false,
       right: null
     };
   }
