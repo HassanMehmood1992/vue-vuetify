72d1d80cc5ba0b108e7c8d5a115532dcbde24f1c
Hassan Mehmood <hassan.mehmood@click.aero>
2019-05-05 11:56:00 +0400

    side nav bar updated

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <nav>
       <v-toolbar dark color="primary" flat app>
-        <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-side-icon  class="hidden-md-and-up ml-0" @click="drawer = !drawer"></v-toolbar-side-icon>
 
         <v-toolbar-title class="white--text">Title</v-toolbar-title>
 
@@ -24,22 +24,18 @@
           <v-icon>more_vert</v-icon>
         </v-btn>
       </v-toolbar>
-    <v-navigation-drawer app v-model="drawer" class="pb-0" floating hide-overlay stateless width="380">
-      <v-toolbar dark color="primary" flat>
-       
-
-        <v-toolbar-title class="white--text">Logs</v-toolbar-title>
-      </v-toolbar>
-      <v-layout >
-               
-        <v-list class="grow">
-          <v-list-tile v-for="link in links" :key="link" @click>
-            <v-list-tile-title v-text="link"></v-list-tile-title>
-          </v-list-tile>
-        </v-list>
-      </v-layout>
-    </v-navigation-drawer>
-
+      <v-navigation-drawer  app v-model="drawer" class="pb-0" floating  >
+        <v-toolbar dark color="primary" flat>
+          <v-toolbar-title class="white--text">Logs</v-toolbar-title>
+        </v-toolbar>
+        <v-layout>
+          <v-list class="grow">
+            <v-list-tile v-for="link in links" :key="link" @click>
+              <v-list-tile-title v-text="link"></v-list-tile-title>
+            </v-list-tile>
+          </v-list>
+        </v-layout>
+      </v-navigation-drawer>
     </nav>
     <v-content>
       <router-view/>
@@ -63,7 +59,7 @@ export default {
         { title: "About", icon: "question_answer" }
       ],
       links: ["Home", "Contacts", "Settings"],
-      mini: false,
+      mini: true,
       right: null
     };
   }
