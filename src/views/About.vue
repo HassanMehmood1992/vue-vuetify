dafdab219b32beb6f610a5aaf7ea09e5fc156d69
Hassan Mehmood <hassan.mehmood@click.aero>
2019-05-05 13:16:46 +0400

    about layout added

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="m" style="margin-top:8%">
+     <v-layout>
+    <v-flex xs12 sm10 offset-sm1 >
+      <v-card>
+        <v-img
+          src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+          aspect-ratio="2.75"
+        ></v-img>
+
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+            <div> {{ card_text }} </div>
+          </div>
+        </v-card-title>
+
+        <v-card-actions>
+          <v-btn flat color="orange">Share</v-btn>
+          <v-btn flat color="orange">Explore</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
   </div>
 </template>
